b482deaf413b094201866f647443204db8a9c7a0
pratik rai <pratikkumar399@gmail.com>
2025-12-20 19:51:37 +0530

    google tag to only be enabled on prod

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -115,18 +115,22 @@ export default function RootLayout({
         className={`${inter.variable} ${outfit.variable} ${firaCode.variable} antialiased bg-dark-bg text-dark-text`}
         suppressHydrationWarning
       >
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-J9C1KGRP3Z"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-J9C1KGRP3Z');
-          `}
-        </Script>
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-J9C1KGRP3Z"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-J9C1KGRP3Z');
+              `}
+            </Script>
+          </>
+        )}
         <PWARegister />
         <Providers>
           <AppShell>{children}</AppShell>
